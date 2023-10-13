0bc6ad42908f8204f7bdaa283ed161443a91a41e
minakshijaybhay <minakshi.nj@gmail.com>
2023-10-13 17:52:12 -0500

    added sub function

--- Demo/src/Local.java
+++ Demo/src/Local.java
@@ -1,5 +1,9 @@
 
 public class Local {
+	
+	public void sub() {
+		System.out.println("Subtraction is done");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -7,6 +11,9 @@ public class Local {
 		System.out.println("Hi");
 		System.out.println("hello");
 		System.out.println("Hi changes are done");
+		
+		
+		
 
 	}
 
